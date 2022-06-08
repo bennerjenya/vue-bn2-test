788c11b7e37095318dbdde09e325610fe7fddbe9
bennerjenya <bennerov@mail.ru>
2022-06-09 00:49:29 +0600

    fixes

--- components/TreeItem.vue
+++ components/TreeItem.vue
@@ -1,10 +1,10 @@
 <template>
-  <li>
-    <div class="menu-link-holder" :class="{ bold: isSubmenu }" @click="toggle">
+  <li class="item">
+    <div class="menu-link-holder" :class="{ bold: isSubmenu }">
       <a class="menu-link" :to="`${fullLink}`">
         {{ model.name }}
       </a>
-      <span v-if="isSubmenu" class="arrow" :class="(isOpen ? 'toggled' : '')">
+      <span @click="toggle" v-if="isSubmenu" class="arrow" :class="(isOpen ? 'toggled' : '')">
         <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path fill-rule="evenodd" clip-rule="evenodd"
                 d="M12 14.0859L5.70706 7.79297L4.29285 9.20718L12 16.9143L19.7071 9.20718L18.2928 7.79297L12 14.0859Z"
@@ -17,7 +17,6 @@
       <TreeItem
         v-for="(item, index) in model.submenu"
         :key="index"
-        class="item"
         :model="item"
         :link-start="fullLink"
         :link-end="model.slug"
@@ -91,13 +90,16 @@ export default {
     transition: max-height 0.3s;
     &--opened {
       max-height: 1000px;
-      transition-delay: 150ms;
     }
   }
 }
 .menu-link {
   font-size: 14px;
   line-height: 18px;
+  cursor: pointer;
+  &:hover {
+    color: #c4c4c4;
+  }
 }
 
 .menu-link-holder {
@@ -110,6 +112,7 @@ export default {
   display: flex;
   align-items: center;
   justify-content: center;
+  cursor: pointer;
   & svg {
     transition: all .3s linear;
   }
